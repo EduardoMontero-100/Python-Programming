3665b9a123179920c287bdfa91b5df919c081ac4
EduardoMontero-100 <ejmontero10@gmail.com>
2022-04-29 23:34:13 -0300

    add scripts

---
+++ Python-Basics/coderhouse/Clase 15 - Módulos y Paquetes/Archivos de los Ejemplos del PPT/ejemploModulos/uso.py
@@ -0,0 +1,11 @@
+import funciones_matematicas 
+from atleta import *
+
+
+funciones_matematicas.sumar(4,11)
+
+
+
+atleta_nuevo = TriAtleta("Mariposa", "500", "Urbano", "BMX", "Nicolas Perez", 31)
+
+print(f"{atleta_nuevo}")
